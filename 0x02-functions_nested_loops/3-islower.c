62a73e6884a44de73c2a6300da949380b2d3e3aa
33kip <jkiplimo33@gmail.com>
2023-07-18 11:46:54 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,41 +1,16 @@
-#include <unistd.h> // For write and STDIN_FILENO
 #include "main.h"
 
 /**
- * is_lowercase - Checks if a character is lowercase.
- * @c: The character to check.
- *
- * Return: 1 if @c is lowercase, 0 otherwise.
- */
-int is_lowercase(int c)
+* is_lowercase - Checks if a character is lowercase.
+* @c: The character to check.
+*
+* Return: 1 if @c is lowercase, 0 otherwise.
+*/
+int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
+    if (c >= 97 && c <= 122)
     {
         return (1);
     }
-    return (0);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char input;
-    int result;
-
-    /* Read a character from the user */
-    write(STDOUT_FILENO, "Enter a character: ", 19);
-    read(STDIN_FILENO, &input, 1);
-
-    /* Check if the character is lowercase */
-    result = is_lowercase(input);
-
-    /* Print the result */
-    putchar(result + '0');
-    putchar('\n');
-
     return (0);
 }
