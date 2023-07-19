c31f3e6a54563276e9aca10ee151669e8a4de978
33kip <jkiplimo33@gmail.com>
2023-07-19 14:08:36 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void print_fibonacci_numbers(int n) {
+    int fib[n];
+    fib[0] = 1;
+    fib[1] = 2;
+
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", fib[i]);
+        if (i != n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n = 50;
+    print_fibonacci_numbers(n);
+    return 0;
+}
