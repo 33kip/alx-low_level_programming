db03ff644c721c39ce12640850786993195549e4
33kip <jkiplimo33@gmail.com>
2023-07-24 04:52:45 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - Prints a string followed by a new line.
+* @str: The input string to be printed.
+* Return: String and new line
+*/
+void _puts(char *str)
+{
+if (str != NULL)
+{
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n')
+}
+}
