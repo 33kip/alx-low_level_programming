216810039ee743190a27e6fc04e8b169aca2b649
33kip <jkiplimo33@gmail.com>
2023-07-25 06:36:34 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,25 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 32
+#define PASSWORD_LENGTH 15
 
-int main(void)
-{
-    char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    char password[PASSWORD_LENGTH + 1];
-    int i;
+char *generate_random_password() {
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char *password = (char *)malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+
+    if (password == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
 
     srand(time(NULL));
 
-    for (i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        int index = rand() % (sizeof(valid_chars) - 1);
-        password[i] = valid_chars[index];
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
     }
 
     password[PASSWORD_LENGTH] = '\0';
 
+    return password;
+}
+
+int main(void) {
+    char *password = generate_random_password();
+
     printf("%s\n", password);
 
+    free(password);
     return 0;
 }
