bd8cd9563c81104ee8c18aeac8719ab6939e7d2b
33kip <jkiplimo33@gmail.com>
2023-09-04 07:02:39 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+
+void display_elf_header(Elf64_Ehdr *header) {
+    int i;
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", header->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             ");
+    if (header->e_ident[EI_CLASS] == ELFCLASS32) {
+        printf("ELF32\n");
+    } else if (header->e_ident[EI_CLASS] == ELFCLASS64) {
+        printf("ELF64\n");
+    } else {
+        printf("Invalid\n");
+    }
+    printf("  Data:                              ");
+    if (header->e_ident[EI_DATA] == ELFDATA2LSB) {
+        printf("2's complement, little endian\n");
+    } else if (header->e_ident[EI_DATA] == ELFDATA2MSB) {
+        printf("2's complement, big endian\n");
+    } else {
+        printf("Invalid\n");
+    }
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            ");
+    switch (header->e_ident[EI_OSABI]) {
+        case ELFOSABI_SYSV:
+            printf("UNIX - System V\n");
+            break;
+        case ELFOSABI_HPUX:
+            printf("HP-UX\n");
+            break;
+        case ELFOSABI_NETBSD:
+            printf("NetBSD\n");
+            break;
+        case ELFOSABI_LINUX:
+            printf("Linux\n");
+            break;
+        case ELFOSABI_SOLARIS:
+            printf("Solaris\n");
+            break;
+        case ELFOSABI_AIX:
+            printf("AIX\n");
+            break;
+        case ELFOSABI_IRIX:
+            printf("IRIX\n");
+            break;
+        case ELFOSABI_FREEBSD:
+            printf("FreeBSD\n");
+            break;
+        case ELFOSABI_TRU64:
+            printf("TRU64 UNIX\n");
+            break;
+        case ELFOSABI_MODESTO:
+            printf("Novell Modesto\n");
+            break;
+        case ELFOSABI_OPENBSD:
+            printf("OpenBSD\n");
+            break;
+        case ELFOSABI_ARM_AEABI:
+            printf("ARM EABI\n");
+            break;
+        case ELFOSABI_ARM:
+            printf("ARM\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              ");
+    switch (header->e_type) {
+        case ET_NONE:
+            printf("NONE (Unknown)\n");
+            break;
+        case ET_REL:
+            printf("REL (Relocatable file)\n");
+            break;
+        case ET_EXEC:
+            printf("EXEC (Executable file)\n");
+            break;
+        case ET_DYN:
+            printf("DYN (Shared object file)\n");
+            break;
+        case ET_CORE:
+            printf("CORE (Core file)\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("  Entry point address:               %#lx\n", header->e_entry);
+}
+
+int main(int argc, char *argv[]) {
+    int fd;
+    Elf64_Ehdr header;
+    ssize_t bytes_read;
+
+    if (argc != 2) {
+        dprintf(2, "Usage: %s elf_filename\n", argv[0]);
+        return (98);
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        dprintf(2, "Error: Can't open file %s\n", argv[1]);
+        return (98);
+    }
+
+    bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
+    if (bytes_read == -1 || bytes_read != sizeof(Elf64_Ehdr)) {
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+        close(fd);
+        return (98);
+    }
+
+    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+        header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 ||
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        dprintf(2, "Error: Not an ELF file: %s\n", argv[1]);
+        close(fd);
+        return (98);
+    }
+
+    display_elf_header(&header);
+    close(fd);
+    return (0);
+}
+
